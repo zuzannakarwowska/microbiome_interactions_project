0fbc33f26c44558cedb572ddca2a53147c326d0d
Paweł Szczerbiak <pszczerbiak@metagenomic-deepfri-v1.mcb.bio>
2022-03-31 11:13:01 +0200

    Demo notebook updated

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -25,25 +25,25 @@ def plot_series(train_true, train_pred, train_naive,
     if train_true is not None and train_pred is not None:
         plt.figure(figsize=(15,5))
         plt.subplot(1, 2, 1)
-        plt.plot(train_true[:, group-1], label='true')
-        plt.plot(train_pred[:, group-1], label='predicted')
+        plt.plot(train_true[group-1], label='true')
+        plt.plot(train_pred[group-1], label='predicted')
         if train_naive is not None:
-            plt.plot(train_naive[:, group-1], label='naive')
-        # plt.plot(abs(test_inv_yhat_super[:, group-1] - \
-        #              test_inv_y[:, group-1]))  # absolute difference
-        rmse = np.sqrt(mean_squared_error(train_true[:, group-1], 
-                                          train_pred[:, group-1]))
+            plt.plot(train_naive[group-1], label='naive')
+        # plt.plot(abs(test_inv_yhat_super[group-1] - \
+        #              test_inv_y[group-1]))  # absolute difference
+        rmse = np.sqrt(mean_squared_error(train_true[group-1], 
+                                          train_pred[group-1]))
         plt.title(f'Train (group: {group}, RMSE: {rmse:.2f})')
         plt.legend()
         plt.subplot(1, 2, 2)
-    plt.plot(test_true[:, group-1], label='true')
-    plt.plot(test_pred[:, group-1], label='predicted')
+    plt.plot(test_true[group-1], label='true')
+    plt.plot(test_pred[group-1], label='predicted')
     if train_naive is not None:
-        plt.plot(test_naive[:, group-1], label='naive')
-    # plt.plot(abs(test_inv_yhat_super[:, group-1] - \
-    #              test_inv_y[:, group-1]))  # absolute difference
-    rmse = np.sqrt(mean_squared_error(test_true[:, group-1],
-                                      test_pred[:, group-1]))
+        plt.plot(test_naive[group-1], label='naive')
+    # plt.plot(abs(test_inv_yhat_super[group-1] - \
+    #              test_inv_y[group-1]))  # absolute difference
+    rmse = np.sqrt(mean_squared_error(test_true[group-1],
+                                      test_pred[group-1]))
     plt.title(f'Test (group: {group}, RMSE: {rmse:.2f})')
     plt.legend()
     plt.show()
